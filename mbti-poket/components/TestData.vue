740bded43afb6423425694aee552c66cae99e1ce
yongmin <ym7596@naver.com>
2022-02-24 18:04:46 +0900

    ss

--- mbti-poket/components/TestData.vue
+++ mbti-poket/components/TestData.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
-        <ul v-for="(item,index) in monlist" :key = "index">
-            <li>{{ item.Name }} </li>
-            <li>{{ item.Name_ko }} </li>
-            <li>{{ item.Num }} </li>
-        </ul>
+      
+            <v-card
+    class="mx-auto"
+    max-width="344"
+    v-for="(item,index) in monlist" :key = "index"
+  >
+    <v-img
+      src= {{ item.imgsrc }}
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    
+  </v-card>
+        
     </div>
 </template>
 <script>
